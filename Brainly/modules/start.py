2160519a30e41bd511e4425b8b98201e0f614d99
Barklei <81870902+nekozu@users.noreply.github.com>
2021-08-01 20:57:33 +0700

    Update start.py

--- Brainly/modules/start.py
+++ Brainly/modules/start.py
@@ -3,7 +3,8 @@ from pyrogram import Client, filters
 @Client.on_message(filters.command(["start"]))
 async def start(bot, update):
     await bot.send_message(
-        chat_id=update.chat.id,
-        text="Halo Aku Adalah Bot Untuk Mencari Dibrainly!, Ketik /cari pertanyaan untuk mencari pertanyaan, Bisa menggunakan inline juga loh! Contoh @brainlynbot cari siapa penemu sepeda? .",
-        reply_to_message_id=update.message_id
+        chat_id=update.chat.id,                                                                                                                                                                                                 
+        text="Halo Aku Adalah Bot Untuk Mencari Dibrainly!, Ketik /cari pertanyaan untuk mencari pertanyaan, Bisa menggunakan inline juga loh! Contoh `@brainlynbot cari siapa penemu sepeda?` .",
+        reply_to_message_id=update.message_id,
+        parse_mode="markdown"
     )
