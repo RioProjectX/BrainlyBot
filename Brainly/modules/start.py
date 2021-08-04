254e4cd3b243545da755aac1fa1faa2081a9be2b
Barklei <81870902+nekozu@users.noreply.github.com>
2021-08-04 16:12:33 +0700

    w

--- Brainly/modules/start.py
+++ Brainly/modules/start.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,                                                                                                                                                                                                 
-        text="Halo Aku Adalah Bot Untuk Mencari Dibrainly!, Ketik /cari pertanyaan untuk mencari pertanyaan, Bisa menggunakan inline juga loh! Contoh `@brainlynbot cari siapa penemu sepeda?` .",
+        text="**Halo Aku Adalah Bot Untuk Mencari Dibrainly!**, Ketik /cari pertanyaan untuk mencari pertanyaan, Bisa menggunakan inline juga loh! Contoh `@brainlynbot cari siapa penemu sepeda?` .",
         reply_to_message_id=update.message_id,
         parse_mode="markdown"
     )
