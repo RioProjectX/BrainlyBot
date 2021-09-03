9f360860ff6624bab36f86a0bb4f3633fb138b96
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-09-03 13:01:50 +0700

    Update start.py

--- Brainly/modules/start.py
+++ Brainly/modules/start.py
@@ -5,6 +5,13 @@ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,                                                                                                                                                                                                 
         text="**Halo Saya Adalah Bot Untuk Mencari Jawaban Dibrainly , Saya Dibuat Oleh @Riio00 !**, Ketik /cari pertanyaan untuk mencari pertanyaan, Bisa menggunakan inline juga loh! Contoh @brainlyriobot cari siapa penemu sepeda?` .",
-        reply_to_message_id=update.message_id,
-        parse_mode="markdown"
-    )
+        reply_markup=InlineKeyboardMarkup(
+                       [[
+                          InlineKeyboardButton(
+                             "👑Developer", url="https://t.me/riio00")
+                       ],[
+                          InlineKeyboardButton(
+                             "🌐 Channel", url="https://t.me/riobotsupport")
+                       ]]
+
+                    ))
