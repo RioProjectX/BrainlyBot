04d0ecd61790a12e194248ee4ff2feea26776509
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-10-06 13:47:26 +0700

    Update start.py

--- Brainly/modules/start.py
+++ Brainly/modules/start.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, 
 async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,                                                                                                                                                                                                 
-        text="**Halo Saya Adalah Bot Untuk Mencari Jawaban Di [brainly](https://telegra.ph/file/ae2101aa6bed02b9d27f2.jpg) , Saya Dibuat Oleh @fckualot !**, Ketik /cari pertanyaan untuk mencari pertanyaan, Bisa menggunakan inline juga loh! Contoh @brainlyriobot cari siapa penemu sepeda?` .",
+        text="**Halo Saya Adalah Bot Untuk Mencari Jawaban Di [brainly](https://telegra.ph/file/ae2101aa6bed02b9d27f2.jpg) , Saya Dibuat Oleh @fckualot !**, Ketik /cari pertanyaan untuk mencari pertanyaan, Bisa menggunakan inline juga loh! Contoh @riobrainlybot cari siapa penemu sepeda?` .",
         reply_markup=InlineKeyboardMarkup(
                        [[
                           InlineKeyboardButton(
